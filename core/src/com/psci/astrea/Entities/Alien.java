cb16075bfedc5d27d910b7ff803201c4c421011a
bilalzahid26 <bilal.zahid99@yahoo.com>
2017-09-29 14:58:54 +0100

    stuff

---
+++ core/src/com/psci/astrea/Entities/Alien.java
@@ -0,0 +1,20 @@
+package com.psci.astrea.Entities;
+
+public class Alien {
+
+    public void movement(){
+
+
+    }
+
+    public void rotation(){
+
+    }
+
+    public void collision(){
+
+
+    }
+
+    public void shoot(){}
+}
