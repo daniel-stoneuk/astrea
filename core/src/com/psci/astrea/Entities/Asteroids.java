81389857082d1dd4fa8199e9beb81ef229c30b5d
bilalzahid26 <bilal.zahid99@yahoo.com>
2017-09-29 14:42:48 +0100

    Added classes for entities

---
+++ core/src/com/psci/astrea/Entities/Asteroids.java
@@ -0,0 +1,24 @@
+package com.psci.astrea.Entities;
+
+public class Asteroids {
+
+    public void movement(){
+
+
+    }
+
+    public void rotation(){
+
+    }
+
+    public void collision(){
+
+
+    }
+
+    public void gravity(){}
+
+    public void image(){}
+
+
+}
