8ae748341909c571600e6e65490f74fd6d5ed9f1
udeshya <udeshya@hotmail.co.uk>
2017-10-01 07:47:52 +0100

    Added box2dlight support

--- core/src/com/psci/astrea/entity/Star.java
+++ core/src/com/psci/astrea/entity/Star.java
@@ -1,15 +1,19 @@
 package com.psci.astrea.entity;
 
+import box2dLight.*;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.psci.astrea.astrea.Astrea;
 import com.psci.astrea.astrea.MySprite;
 import com.psci.astrea.astrea.SpriteManager;
 import com.psci.astrea.entity.Sun.newsun;
 
 public class Star extends Entity {
 
+    //public RayHandler raay = new RayHandler(Astrea);
 
 
     public static Star create(){
@@ -19,6 +23,9 @@ public class Star extends Entity {
         Star star = new newsun(starSprite, position);
 
         return star;
+    }
+    public void update(float delta) {
+        super.update(delta);
     }
 
     public Star(MySprite sprite, Vector2 position) {
